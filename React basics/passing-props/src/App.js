41c23b34f4c5edd07708cc13df0a7d130e2c8ed1
OanhBui <oanhbp3105@gmail.com>
2023-02-03 22:12:02 -0800

    passing props

---
+++ React basics/passing-props/src/App.js
@@ -0,0 +1,12 @@
+import Heading from "./Heading";
+
+function App() {
+    return (
+        <div className="App">
+            <Heading firstName="Mary" />
+            <Heading firstName="Oanh"/>
+        </div>
+    );
+};
+
+export default App;
